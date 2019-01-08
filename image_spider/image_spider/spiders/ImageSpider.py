66926091ef8eb9be340ea463c4a35dc0230c242f
王言斌 <wangyanbin@iyunbao.com>
2019-01-08 17:17:40 +0800

    filter 'javascript:' 、set LOG_LEVEL

--- image_spider/image_spider/spiders/ImageSpider.py
+++ image_spider/image_spider/spiders/ImageSpider.py
@@ -5,7 +5,6 @@
 # @Desc ImageSpider.py a image spider to fetch images from the specific domain (urls)
 import scrapy
 from scrapy.http import Request
-from scrapy.http.response.html import HtmlResponse
 
 
 class ImageSpider(scrapy.Spider):
@@ -18,6 +17,18 @@ class ImageSpider(scrapy.Spider):
     ]
     URL = []
 
+    def parse(self, response):
+        self.logger.info('a response from %s just arrived!', response.url)
+        print(response.xpath("//img/@src").extract())
+        for href_url in response.xpath("//a/@href").extract():
+            if href_url in self.URL:
+                continue
+            else:
+                self.URL.append(response.url)
+
+            url = self.parse_href_url(response.url, href_url)
+            yield Request(url, callback=self.parse)
+
     @staticmethod
     def get_current_domain(url):
         return '/'.join(url.split('/')[0:3])
@@ -26,15 +37,19 @@ class ImageSpider(scrapy.Spider):
         need_domain = False
         current_domain = self.get_current_domain(current_url)
 
+        # check and replace href_url if necessary.
         if '../' in href_url:
             need_domain = True
             href_url = href_url.replace("../", "")
         if './' in href_url:
             need_domain = True
             href_url = href_url.replace("./", "")
+        if "javascripts:" in href_url:
+            href_url = current_domain
         if need_domain:
             href_url = current_domain + "/" + href_url
 
+        # parse real url
         if href_url.startswith('/'):
             href_url = '/'.join(current_domain) + href_url
         elif href_url.startswith('http') or href_url.startswith('www'):
@@ -45,17 +60,4 @@ class ImageSpider(scrapy.Spider):
                 href_url = current_url + href_url
             else:
                 href_url = '/'.join(current_url.split('/')[0:-1]) + '/' + href_url
-
-        if "javascripts:" in href_url:
-            return current_domain
         return href_url
-
-    def parse(self, response):
-        self.logger.info('A response from %s just arrived!', response.url)
-        for href_url in response.xpath("//a/@href").extract():
-            if href_url in self.URL:
-                continue
-            else:
-                self.URL.append(response.url)
-            url = self.parse_href_url(response.url, href_url)
-            yield Request(url, callback=self.parse)
